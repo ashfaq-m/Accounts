4ef2def7dfdae126b0e4e6d9080f175980acb182
ashfaq-m <ashapak.makandar7@gmail.com>
2025-01-07 14:52:39 +0530

    Create API created

---
+++ src/main/java/com/easybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,15 @@
+package com.easybytes.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
