aed9fd74440bcde662aa951d21c78a5962e33caa
ashfaq-m <ashapak.makandar7@gmail.com>
2025-01-08 12:01:11 +0530

    Till Auditing

--- src/main/java/com/easybytes/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/easybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -35,8 +35,7 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already exists with given mobile number "
             +customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
+
         Customer savedCustomer = customerRepository.save(customer);
         accountRepository.save(createNewAccount(savedCustomer));
     }
@@ -53,8 +52,7 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+
         return newAccount;
     }
 
